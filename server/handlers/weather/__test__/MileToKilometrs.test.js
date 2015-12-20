cce00973f066068380bd2870f7969ee1cf392b69
EugeneIvchenko <ivchenko_eugene@ukr.net>
2015-12-20 21:57:26 +0200

    fix test

--- server/handlers/weather/__test__/MileToKilometrs.test.js
+++ server/handlers/weather/__test__/MileToKilometrs.test.js
@@ -1,13 +1,13 @@
 /**
  * Created by Eugene on 20.12.2015.
  */
-var conv = require('../MileToKilometrs');
+var ff = require('../MileToKilometrs');
 
-describe('Convert_speed', function () {
+describe('Convert speed', function () {
 
     it('should convert M to Km', function () {
-        expect(conv(1).toFixed(2)).to.be('1.61');
-        expect(conv(10).toFixed(2)).to.be('16,09');
-        expect(conv(0).toFixed(2)).to.be('0.00');
+        expect(ff(1).toFixed(2)).to.be('1.61');
+        expect(ff(10).toFixed(2)).to.be('16.09');
+        expect(ff(0).toFixed(2)).to.be('0.00');
     });
 });
--- server/handlers/weather/__test__/apiParser.test.js
+++ server/handlers/weather/__test__/apiParser.test.js
@@ -6,7 +6,7 @@ describe('Api parser', function () {
   it('should return Date from timestamp', function () {
     var expectedDate = new Date();
 
-    var a = parser({ currently: { time: toUnixTimestamp(expectedDate), temperature: 1 } });
+    var a = parser({ currently: { time: toUnixTimestamp(expectedDate), temperature: 1, windSpeed: 1} });
 
     expect(a.currentDate).to.be.a(Date);
     expect(a.currentDate.getTime()).to.be(expectedDate.getTime());
